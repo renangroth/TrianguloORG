acf0d46a67039e0b71042008892b480fa1d20ecb
Renan Groth <31290593+renangroth@users.noreply.github.com>
2019-10-29 10:18:43 -0300

    Add files via upload

--- triangulo.asm
+++ triangulo.asm
@@ -1,13 +1,10 @@
 	.data
 
-msg_N: 	 	.string "Digite valor de n: (Maior ou igual a 1) "
-msg_K: 	 	.string "Digite valor de k: (Maior ou igual a 1 e menor ou igual que N)"
-msg_nvLinha:	.string "\n"
-msg_spaco:	.string " "
+msg_N: 	 	.string "Digite valor de n: (Maior ou igual a 1)\n"
+msg_K: 	 	.string "Digite valor de k: (Maior ou igual a 1 e menor ou igual que N)\n"
 
 	.text
 
-main:
 		addi s0, zero, 0	#s0 vai ser N
 		addi s1, zero, 0	#s1 vai ser K
 		addi t0, zero, 1    #t0 vale 1
@@ -16,9 +13,6 @@ pedeN:	la	a0, msg_N		#Carrega mensagem em a0
 		ecall				#Imprime
 		li s0, 5			#Pega o Int digitado
 		blt s0, t0, pedeN   #Testa se N < 1
-		la	a0, msg_nvLinha #Carrega mensagem em a0
-		li 	a7, 4			#Seta para imprimir String
-		ecall				#Imprime
 pedeK:	la	a0, msg_K		#Carrega mensagem em a0
 		li 	a7, 4			#Seta para imprimir String
 		ecall				#Imprime
@@ -26,7 +20,5 @@ pedeK:	la	a0, msg_K		#Carrega mensagem em a0
 		blt s1, t0, pedeK	#Testa se K < 1
 		bge s1, s0, pedeK	#Testa se K>=N
 
-ST_2: 	addi s0, s0, 0
-		addi s1, s1, 0
-		ret
+
 	
